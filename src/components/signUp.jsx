02a0035ecf94b98bd73931ec33eef1bd09dc89df
ABC Man <Shevchikkola@gmail.com>
2022-08-24 17:02:46 +0300

    fix user registration

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -11,42 +11,53 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import CloseIcon from '@mui/icons-material/Close';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import {createUserWithEmailAndPassword, getAuth} from "firebase/auth";
-import {auth,connectAuthEmulator, signInWithEmailAndPassword} from "firebase/auth";
-import {connect} from "react-redux";
-// import  {useState} from "react";
-
+import {useDispatch} from "react-redux";
+import {Alert, Collapse} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import {getCurrentUserAction} from "../store/userReducer";
+import {changeAuthAction} from "../store/authReducer";
+import {useNavigate} from "react-router-dom";
 
 const theme = createTheme();
 
 export default function SignUp() {
+    const [open, setOpen] = React.useState(false);
+    const dispatch = useDispatch();
+    let navigate = useNavigate();
 
-
+    const checkAuth = () => {
+        dispatch(changeAuthAction(true))
+        let path = `/`;
+        navigate(path);
+    }
     const handleSubmit = async (event) => {
         event.preventDefault()
         const data = new FormData(event.currentTarget);
+        console.log(data)
         let newUser = {
+            userId: Date.now(),
             firstName: data.get('firstName'),
             lastName: data.get('lastName'),
             email: data.get('email'),
             password: data.get('password'),
         }
-        const auth = getAuth();
-        connectAuthEmulator(auth, "http://localhost:9099")
-       const userCredential = await signInWithEmailAndPassword(auth, newUser.email, newUser.password)
-        console.log(userCredential)
-        let parsedString;
-        if (localStorage.getItem('UserList') !== null) {
-            parsedString = JSON.parse(localStorage.getItem('UserList'))
-            newUser.id = parsedString.length
-            console.log(parsedString)
-            parsedString.push(newUser)
-            console.log(parsedString)
-            localStorage.setItem('UserList', JSON.stringify(parsedString))
+        if (newUser.firstName.length === 0 || newUser.lastName.length === 0 || newUser.email.length === 0 || newUser.password.length === 0) {
+            setOpen(true)
         } else {
-            newUser.id = 0;
-            localStorage.setItem('UserList', JSON.stringify([newUser]))
+            let parsedString;
+            if (localStorage.getItem('UserList') !== null) {
+                parsedString = JSON.parse(localStorage.getItem('UserList'))
+                // newUser.id = parsedString.length
+                parsedString.push(newUser)
+                localStorage.setItem('UserList', JSON.stringify(parsedString))
+            } else {
+                newUser.id = 0;
+                localStorage.setItem('UserList', JSON.stringify([newUser]))
+            }
+            dispatch(getCurrentUserAction(newUser))
+            await checkAuth()
         }
     };
 
@@ -68,6 +79,26 @@ export default function SignUp() {
                     <Typography component="h1" variant="h5">
                         Sign up
                     </Typography>
+                    <Collapse in={open}>
+                        <Alert
+                            severity="error"
+                            action={
+                                <IconButton
+                                    aria-label="close"
+                                    color="inherit"
+                                    size="small"
+                                    onClick={() => {
+                                        setOpen(false);
+                                    }}
+                                >
+                                    <CloseIcon fontSize="inherit"/>
+                                </IconButton>
+                            }
+                            sx={{mb: 2}}
+                        >
+                            Please fill in the fields
+                        </Alert>
+                    </Collapse>
                     <Box component="form" noValidate onSubmit={handleSubmit} sx={{mt: 3}}>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
