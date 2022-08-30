21516bd85c34bd58b643059737128fd2a996b07d
ABC Man <Shevchikkola@gmail.com>
2022-08-30 12:50:58 +0300

    rename action

--- src/store/todoReducer.js
+++ src/store/todoReducer.js
@@ -5,14 +5,14 @@ const defaultState = {
     todos: JSON.parse(localStorage.getItem('todoItems'))
 }
 const ADD_TODO = "ADD_TODO"
-const REMOVE_CUSTOMER = "REMOVE_CUSTOMER"
+const REMOVE_TODO = "REMOVE_TODO"
 const EDIT_TODO = "EDIT_TODO"
 
 export const todoReducer = (state = defaultState, action) => {
     switch (action.type){
         case ADD_TODO:
             return {...state, todos: [...state.todos, action.payload]}
-        case REMOVE_CUSTOMER:
+        case REMOVE_TODO:
             return {...state, todos: state.todos.filter((todo)=> todo.id !== action.payload)}
         case EDIT_TODO:
             return {...state, todos: state.todos.map((todo, index,) => {
@@ -31,5 +31,5 @@ export const todoReducer = (state = defaultState, action) => {
     }
 }
 export const addTodoAction = (payload) => ({type:ADD_TODO, payload})
-export const removeTodoAction = (payload) => ({type:REMOVE_CUSTOMER, payload})
+export const removeTodoAction = (payload) => ({type:REMOVE_TODO, payload})
 export const editTodoAction = (payload, newTodo) => ({type:EDIT_TODO, payload, newTodo})
