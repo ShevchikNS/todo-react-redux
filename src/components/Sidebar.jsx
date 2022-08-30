b98f60877b3b5d8c8133fd29e8add2fb2331f239
ABC Man <Shevchikkola@gmail.com>
2022-08-30 12:51:56 +0300

    fix sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,15 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from "@mui/icons-material/Menu";
 import FormDialog from "./folderInputForm";
+import {useDispatch, useSelector} from "react-redux";
+import LongMenu from "./menuForFolders";
+import {removeFolderAction} from "../store/folderReducer";
+import {store} from "../store";
 
 export default function Sidebar() {
+    const dispatch = useDispatch()
+    const userFolder = useSelector(state => state.folder.folder)
+    console.log(userFolder)
     const [state, setState] = React.useState({
         left: false
     });
@@ -24,22 +31,35 @@ export default function Sidebar() {
 
         setState({...state, [anchor]: open});
     };
+    const removeFolderItem =async (folderForDelete) => {
+        dispatch(removeFolderAction(folderForDelete.id))
 
+        await localStorage.setItem('todoFolder', JSON.stringify(store.getState().folder.folder))
+
+    }
     const list = (anchor) => (
         <Box
             sx={{width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250}}
             role="presentation"
-            onClick={toggleDrawer(anchor, false)}
+
             onKeyDown={toggleDrawer(anchor, false)}
         >
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemText primary={text}/>
-                        </ListItemButton>
-                    </ListItem>
-                ))}
+                <ListItemButton onClick={toggleDrawer(anchor, false)}>
+                    <ListItemText primary="All"/>
+                </ListItemButton>
+                {
+                    userFolder.length > 0 ?
+                        userFolder.map((folder, index) => (
+                            <ListItem key={folder.folderName} disablePadding>
+                                <ListItemButton onClick={toggleDrawer(anchor, false)}>
+                                    <ListItemText primary={folder.folderName}/>
+                                </ListItemButton>
+                                <LongMenu onDelete={() => removeFolderItem(folder)}/>
+                            </ListItem>
+                        )) : console.log("NEED FIX")
+                }
+
             </List>
             <Divider/>
         </Box>
@@ -56,7 +76,7 @@ export default function Sidebar() {
                         onClose={toggleDrawer(anchor, false)}
                         onOpen={toggleDrawer(anchor, true)}
                     >
-                        <FormDialog />
+                        <FormDialog/>
                         {list(anchor)}
                     </SwipeableDrawer>
                 </React.Fragment>
