085d447562b852b7293c410e13013bf4db7a64e6
ABC Man <Shevchikkola@gmail.com>
2022-08-30 12:48:57 +0300

    add folderReducer

---
+++ src/store/folderReducer.js
@@ -0,0 +1,22 @@
+if (localStorage.getItem("todoFolder") == null)
+    localStorage.setItem('todoFolder', '[]');
+
+const defaultState = {
+    folder: JSON.parse(localStorage.getItem('todoFolder'))
+}
+
+const ADD_FOLDER = "ADD_FOLDER"
+const REMOVE_FOLDER = "REMOVE_FOLDER"
+export const folderReducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case ADD_FOLDER:
+            return {...state, folder: [...state.folder, action.payload]}
+        case REMOVE_FOLDER:
+            return {...state, folder: state.folder.filter((folderItem)=> folderItem.id !== action.payload)}
+        default:
+            return state
+    }
+}
+
+export const addFolderAction = (payload) => ({type: ADD_FOLDER, payload})
+export const removeFolderAction = (payload) => ({type: REMOVE_FOLDER, payload})
--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,12 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import {authReducer} from "./authReducer";
 import {userReducer} from "./userReducer";
+import {folderReducer} from "./folderReducer";
 
 const rootReducer = combineReducers({
     todos: todoReducer,
     auth: authReducer,
-    currentUser: userReducer
+    currentUser: userReducer,
+    folder: folderReducer
 })
 export const store  = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
