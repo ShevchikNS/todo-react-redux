0213bc18cc6b84af069fbbde059c5857d191a4cc
ABC Man <Shevchikkola@gmail.com>
2022-08-30 12:50:04 +0300

    add form for input folder name

--- src/components/folderInputForm.jsx
+++ src/components/folderInputForm.jsx
@@ -7,10 +7,19 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import CreateNewFolderOutlinedIcon from "@mui/icons-material/CreateNewFolderOutlined";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {store} from "../store";
+import {addFolderAction} from "../store/folderReducer";
 
 export default function FormDialog() {
+    const dispatch = useDispatch()
     const [open, setOpen] = React.useState(false);
-
+    const [folder, setFolder] = useState()
+    // store.getState().currentUser.user.userId
+    const changeFolderName = (e) => {
+        setFolder(e.target.value)
+    }
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -18,9 +27,21 @@ export default function FormDialog() {
     const handleClose = () => {
         setOpen(false);
     };
+    const handleAddFolder = async (folder) => {
+        const newFolder = {
+            userId: store.getState().currentUser.user.userId,
+            id: Date.now(),
+            folderName: folder
+        }
+        // dispatch(addFolderAction(newFolder))
+        console.log(dispatch(addFolderAction(newFolder)))
+        await localStorage.setItem('todoFolder', JSON.stringify(store.getState().folder.folder))
+        setFolder('')
+        handleClose()
+    }
 
     return (
-        <div className = "inputFolderName">
+        <div className="inputFolderName">
             <Button sx={{fontSize: 16}}
                     onClick={handleClickOpen}
                     startIcon={
@@ -43,12 +64,14 @@ export default function FormDialog() {
                         type="text"
                         fullWidth
                         variant="standard"
+                        value={folder}
+                        onChange={changeFolderName}
 
                     />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Add</Button>
+                    <Button onClick={async () => handleAddFolder(folder)}>Add</Button>
                 </DialogActions>
             </Dialog>
         </div>
